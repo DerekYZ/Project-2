85a5ba5e1930ef20bce3a24346af6cef0768f0c8
BradMcKn <98128205+BradMcKn@users.noreply.github.com>
2022-02-17 15:46:24 -0500

    Add files via upload
    starting tf's

---
+++ providers.tf
@@ -0,0 +1,15 @@
+#providers
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.96.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
