a082dd64a722d2f87640f8a9966ec955f31a296e
DerekYZ <yijun.zhao_2020@outlook.com>
2022-02-23 14:31:53 -0800

    debug LB

--- main.tf
+++ main.tf
@@ -64,25 +64,25 @@ resource "azurerm_subnet" "subnet1" {
   name                 = var.subnet1
   resource_group_name  = azurerm_resource_group.trg1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefix     = var.subnet1_address
+  address_prefix       = var.subnet1_address
 }
 resource "azurerm_subnet" "subnet2" {
   name                 = var.subnet2
   resource_group_name  = azurerm_resource_group.trg1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefix     = var.subnet2_address
+  address_prefix       = var.subnet2_address
 }
 resource "azurerm_subnet" "subnet3" {
   name                 = var.subnet3
   resource_group_name  = azurerm_resource_group.trg1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefix     = var.subnet3_address
+  address_prefix       = var.subnet3_address
 }
 resource "azurerm_subnet" "subnet4" {
   name                 = var.subnet4
   resource_group_name  = azurerm_resource_group.trg1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefix     = var.subnet4_address
+  address_prefix       = var.subnet4_address
 }
 
 #associate NSG to subnets
@@ -103,46 +103,9 @@ resource "azurerm_subnet_network_security_group_association" "associate_nsg_subn
   network_security_group_id = azurerm_network_security_group.Network_Security_Group.id
 }
 
-#VM scale set in web tier/ subnet1
-resource "azurerm_linux_virtual_machine_scale_set" "VMss1" {
-  name                = "VMss1"
-  resource_group_name = azurerm_resource_group.trg1.name
-  location            = azurerm_resource_group.trg1.location
-  sku                 = "Standard_B1s"
-  instances           = 3
-  admin_username      = "azureuser"
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-  }
-
-  network_interface {
-    name    = "example"
-    primary = true
-
-    ip_configuration {
-      name      = "internal"
-      subnet_id = azurerm_subnet.subnet1.id
-    }
-  }
-}
-
 #VM scale set in bussiness tier/ subnet2
-resource "azurerm_linux_virtual_machine_scale_set" "VMss2" {
-  name                = "VMss2"
+resource "azurerm_linux_virtual_machine_scale_set" "VMss" {
+  name                = "VMss"
   resource_group_name = azurerm_resource_group.trg1.name
   location            = azurerm_resource_group.trg1.location
   sku                 = "Standard_B1s"
@@ -176,6 +139,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "VMss2" {
     }
   }
 }
+
 #NIC
 resource "azurerm_network_interface" "r1nic" {
   name                = "region_01_nic"
@@ -186,28 +150,33 @@ resource "azurerm_network_interface" "r1nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet3.id
     private_ip_address_allocation = "Dynamic"
-     public_ip_address_id          = azurerm_public_ip.region_01_PIP.id
+    public_ip_address_id          = azurerm_public_ip.region_01_PIP.id
   }
 }
 #PIP
 resource "azurerm_public_ip" "region_01_PIP" {
   name                = "region_01_PIP"
-  resource_group_name = var.team1rg1
-  location            = var.location
+  resource_group_name = azurerm_resource_group.trg1.name
+  location            = azurerm_resource_group.trg1.location
   allocation_method   = "Dynamic"
 }
+
 #********************************************************FIRST INTERNAL LOAD BALANCER************************************************************
 #Private Load Balancer. (this is the deployment of load balancer )
 resource "azurerm_lb" "Private_Balancer_apps" {
-  name                = "Priavate-Balancer-apps"
-  location            = var.location
-  resource_group_name = var.team1rg1
+  name                = var.private_balancer_apps_name
+  resource_group_name = azurerm_resource_group.trg1.name
+  location            = azurerm_resource_group.trg1.location
 
   frontend_ip_configuration {
-    name                 = "Internal"
+    name                          = var.frontend_ip_configuration_name
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.fip_private_ip_address
   }
 
 }
+
 #deployment of backend address pool
 resource "azurerm_lb_backend_address_pool" "PLB_Backend1" {
   loadbalancer_id = azurerm_lb.Private_Balancer_apps.id
@@ -222,14 +191,14 @@ resource "azurerm_lb_probe" "PrivateLB_Probe1" {
   port                = 22
 }
 #depoloyment of Load Balancer Rule.
-resource "azurerm_lb_rule" "example" {
-  resource_group_name            = var.team1rg1
+resource "azurerm_lb_rule" "lb-rule" {
+  resource_group_name            = azurerm_resource_group.trg1.name
   loadbalancer_id                = azurerm_lb.Private_Balancer_apps.id
   name                           = "LBRule"
   protocol                       = "Tcp"
   frontend_port                  = 22
   backend_port                   = 22
-  frontend_ip_configuration_name = "frontend-IP"
+  frontend_ip_configuration_name = var.frontend_ip_configuration_name
   backend_address_pool_id        = azurerm_lb_backend_address_pool.PLB_Backend1.id
   probe_id                       = azurerm_lb_probe.PrivateLB_Probe1.id
 }
